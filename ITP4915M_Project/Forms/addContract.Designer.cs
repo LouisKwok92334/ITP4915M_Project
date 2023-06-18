e1159128fa7f1024589d6fdb64e5279a838d0a99
boscochuen011 <Bosco.chuen@gmail.com>
2023-06-18 16:34:12 +0800

    update

--- ITP4915M_Project/Forms/addContract.Designer.cs
+++ ITP4915M_Project/Forms/addContract.Designer.cs
@@ -54,9 +54,10 @@
             // 
             // butNext
             // 
-            this.butNext.Location = new System.Drawing.Point(1544, 912);
+            this.butNext.Location = new System.Drawing.Point(950, 570);
+            this.butNext.Margin = new System.Windows.Forms.Padding(2);
             this.butNext.Name = "butNext";
-            this.butNext.Size = new System.Drawing.Size(244, 54);
+            this.butNext.Size = new System.Drawing.Size(150, 34);
             this.butNext.TabIndex = 3;
             this.butNext.Text = "Next";
             this.butNext.UseVisualStyleBackColor = true;
@@ -64,79 +65,88 @@
             // lisItem
             // 
             this.lisItem.FormattingEnabled = true;
-            this.lisItem.ItemHeight = 24;
-            this.lisItem.Location = new System.Drawing.Point(114, 203);
+            this.lisItem.ItemHeight = 15;
+            this.lisItem.Location = new System.Drawing.Point(70, 127);
+            this.lisItem.Margin = new System.Windows.Forms.Padding(2);
             this.lisItem.Name = "lisItem";
-            this.lisItem.Size = new System.Drawing.Size(617, 388);
+            this.lisItem.Size = new System.Drawing.Size(381, 244);
             this.lisItem.TabIndex = 1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(262, 101);
+            this.textBox1.Location = new System.Drawing.Point(161, 63);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(334, 36);
+            this.textBox1.Size = new System.Drawing.Size(207, 25);
             this.textBox1.TabIndex = 6;
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(1544, 62);
+            this.txtID.Location = new System.Drawing.Point(950, 39);
+            this.txtID.Margin = new System.Windows.Forms.Padding(2);
             this.txtID.Name = "txtID";
             this.txtID.ReadOnly = true;
-            this.txtID.Size = new System.Drawing.Size(241, 36);
+            this.txtID.Size = new System.Drawing.Size(150, 25);
             this.txtID.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(109, 106);
+            this.label1.Location = new System.Drawing.Point(67, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 24);
+            this.label1.Size = new System.Drawing.Size(75, 15);
             this.label1.TabIndex = 10;
             this.label1.Text = "Search Item";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1477, 67);
+            this.label5.Location = new System.Drawing.Point(909, 42);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(34, 24);
+            this.label5.Size = new System.Drawing.Size(22, 15);
             this.label5.TabIndex = 15;
             this.label5.Text = "ID";
             // 
             // lisAdd
             // 
             this.lisAdd.FormattingEnabled = true;
-            this.lisAdd.ItemHeight = 24;
-            this.lisAdd.Location = new System.Drawing.Point(1120, 203);
+            this.lisAdd.ItemHeight = 15;
+            this.lisAdd.Location = new System.Drawing.Point(689, 127);
+            this.lisAdd.Margin = new System.Windows.Forms.Padding(2);
             this.lisAdd.Name = "lisAdd";
-            this.lisAdd.Size = new System.Drawing.Size(665, 388);
+            this.lisAdd.Size = new System.Drawing.Size(411, 244);
             this.lisAdd.TabIndex = 16;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(866, 325);
+            this.btnAdd.Location = new System.Drawing.Point(533, 203);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(143, 54);
+            this.btnAdd.Size = new System.Drawing.Size(88, 34);
             this.btnAdd.TabIndex = 17;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(866, 424);
+            this.btnRemove.Location = new System.Drawing.Point(533, 265);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(2);
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(143, 54);
+            this.btnRemove.Size = new System.Drawing.Size(88, 34);
             this.btnRemove.TabIndex = 18;
             this.btnRemove.Text = "Remove";
             this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // rdoPlanned
             // 
             this.rdoPlanned.AutoSize = true;
-            this.rdoPlanned.Location = new System.Drawing.Point(247, 67);
-            this.rdoPlanned.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.rdoPlanned.Location = new System.Drawing.Point(152, 42);
             this.rdoPlanned.Name = "rdoPlanned";
-            this.rdoPlanned.Size = new System.Drawing.Size(160, 28);
+            this.rdoPlanned.Size = new System.Drawing.Size(103, 19);
             this.rdoPlanned.TabIndex = 20;
             this.rdoPlanned.TabStop = true;
             this.rdoPlanned.Text = "Planned P.O.";
@@ -145,10 +155,9 @@
             // rdoContract
             // 
             this.rdoContract.AutoSize = true;
-            this.rdoContract.Location = new System.Drawing.Point(652, 67);
-            this.rdoContract.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.rdoContract.Location = new System.Drawing.Point(401, 42);
             this.rdoContract.Name = "rdoContract";
-            this.rdoContract.Size = new System.Drawing.Size(162, 28);
+            this.rdoContract.Size = new System.Drawing.Size(104, 19);
             this.rdoContract.TabIndex = 19;
             this.rdoContract.TabStop = true;
             this.rdoContract.Text = "Standard P.O";
@@ -157,43 +166,48 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(78, 232);
+            this.label4.Location = new System.Drawing.Point(48, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(110, 24);
+            this.label4.Size = new System.Drawing.Size(70, 15);
             this.label4.TabIndex = 23;
             this.label4.Text = "Total Price";
             // 
             // txtPrice
             // 
-            this.txtPrice.Location = new System.Drawing.Point(247, 227);
+            this.txtPrice.Location = new System.Drawing.Point(152, 142);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(2);
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.ReadOnly = true;
-            this.txtPrice.Size = new System.Drawing.Size(293, 36);
+            this.txtPrice.Size = new System.Drawing.Size(182, 25);
             this.txtPrice.TabIndex = 22;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(78, 74);
+            this.label6.Location = new System.Drawing.Point(48, 46);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(139, 24);
+            this.label6.Size = new System.Drawing.Size(88, 15);
             this.label6.TabIndex = 24;
             this.label6.Text = "Contract Type";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(78, 150);
+            this.label7.Location = new System.Drawing.Point(48, 94);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(86, 24);
+            this.label7.Size = new System.Drawing.Size(55, 15);
             this.label7.TabIndex = 26;
             this.label7.Text = "Supplier";
             // 
             // txtSupplier
             // 
-            this.txtSupplier.Location = new System.Drawing.Point(247, 146);
+            this.txtSupplier.Location = new System.Drawing.Point(152, 91);
+            this.txtSupplier.Margin = new System.Windows.Forms.Padding(2);
             this.txtSupplier.Name = "txtSupplier";
-            this.txtSupplier.Size = new System.Drawing.Size(293, 36);
+            this.txtSupplier.Size = new System.Drawing.Size(182, 25);
             this.txtSupplier.TabIndex = 25;
             // 
             // groupBox1
@@ -206,11 +220,9 @@
             this.groupBox1.Controls.Add(this.rdoPlanned);
             this.groupBox1.Controls.Add(this.txtPrice);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Location = new System.Drawing.Point(114, 643);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.groupBox1.Location = new System.Drawing.Point(70, 402);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.groupBox1.Size = new System.Drawing.Size(1017, 323);
+            this.groupBox1.Size = new System.Drawing.Size(626, 202);
             this.groupBox1.TabIndex = 27;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Contract Info";
@@ -218,10 +230,9 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(479, 67);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.radioButton1.Location = new System.Drawing.Point(295, 42);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 28);
+            this.radioButton1.Size = new System.Drawing.Size(55, 19);
             this.radioButton1.TabIndex = 27;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "BPA";
@@ -229,37 +240,38 @@
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(627, 106);
+            this.btnSearch.Location = new System.Drawing.Point(386, 66);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(106, 35);
+            this.btnSearch.Size = new System.Drawing.Size(65, 22);
             this.btnSearch.TabIndex = 7;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // txtQty
             // 
-            this.txtQty.Location = new System.Drawing.Point(1495, 643);
+            this.txtQty.Location = new System.Drawing.Point(920, 402);
+            this.txtQty.Margin = new System.Windows.Forms.Padding(2);
             this.txtQty.Name = "txtQty";
             this.txtQty.ReadOnly = true;
-            this.txtQty.Size = new System.Drawing.Size(293, 36);
+            this.txtQty.Size = new System.Drawing.Size(182, 25);
             this.txtQty.TabIndex = 28;
-            this.txtQty.TextChanged += new System.EventHandler(this.txtQty_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1373, 655);
+            this.label2.Location = new System.Drawing.Point(845, 409);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.Size = new System.Drawing.Size(56, 15);
             this.label2.TabIndex = 29;
             this.label2.Text = "Quantity";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // addContract
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1895, 1010);
+            this.ClientSize = new System.Drawing.Size(1166, 631);
             this.Controls.Add(this.txtQty);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
@@ -273,9 +285,9 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.butNext);
             this.Controls.Add(this.lisItem);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "addContract";
             this.Text = "Add Contract";
-            this.Load += new System.EventHandler(this.addContract_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
--- ITP4915M_Project/Forms/addContract.cs
+++ ITP4915M_Project/Forms/addContract.cs
@@ -1,35 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.OleDb;
 using System.Windows.Forms;
 
 namespace ITP4915M_Project.Forms
 {
     public partial class addContract : Form
     {
+        private string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=ITP4915.accdb";
+        private List<string> itemNames = new List<string>();
+        private List<string> addedItems = new List<string>();
+
         public addContract()
         {
             InitializeComponent();
+            LoadItems(); // Load the items from the database
         }
 
-        private void addContract_Load(object sender, EventArgs e)
+        private void LoadItems()
         {
+            try
+            {
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    connection.Open();
 
+                    // Load items from the "item" table
+                    string itemQuery = "SELECT item_name FROM item";
+                    OleDbCommand itemCommand = new OleDbCommand(itemQuery, connection);
+                    OleDbDataReader itemReader = itemCommand.ExecuteReader();
+
+                    while (itemReader.Read())
+                    {
+                        string itemName = itemReader.GetString(0); // Assuming item_name is the first column
+                        itemNames.Add(itemName);
+                    }
+
+                    UpdateListBoxes();
+
+                    itemReader.Close();
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while loading items: " + ex.Message);
+            }
         }
 
-        private void txtQty_TextChanged(object sender, EventArgs e)
+        private void UpdateListBoxes()
         {
+            lisItem.DataSource = null;
+            lisItem.DataSource = itemNames;
 
+            lisAdd.DataSource = null;
+            lisAdd.DataSource = addedItems;
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void btnAdd_Click(object sender, EventArgs e)
         {
+            if (lisItem.SelectedItem != null)
+            {
+                string selectedItem = lisItem.SelectedItem.ToString();
 
+                // Prompt the user to enter the quantity
+                string quantity = Prompt.ShowDialog("Enter the quantity for " + selectedItem, "Enter Quantity");
+
+                // Check if quantity is a valid number
+                int quantityNum;
+                if (int.TryParse(quantity, out quantityNum))
+                {
+                    // Add the item with the quantity to the list
+                    addedItems.Add(selectedItem + " - Quantity: " + quantityNum);
+                    itemNames.Remove(selectedItem);
+                    UpdateListBoxes();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid quantity. Please enter a valid number.");
+                }
+            }
         }
+
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lisAdd.SelectedItem != null)
+            {
+                string selectedItem = lisAdd.SelectedItem.ToString();
+
+                // Split the string to remove the quantity
+                string[] parts = selectedItem.Split('-');
+                if (parts.Length > 0)
+                {
+                    string itemName = parts[0].Trim(); // Get the item name, trim is used to remove leading or trailing spaces
+
+                    // Add the item name back to itemNames and remove the item with quantity from addedItems
+                    itemNames.Add(itemName);
+                    addedItems.Remove(selectedItem);
+                    UpdateListBoxes();
+                }
+            }
+        }
+
+        public static class Prompt
+        {
+            public static string ShowDialog(string text, string caption)
+            {
+                Form prompt = new Form()
+                {
+                    Width = 500,
+                    Height = 150,
+                    FormBorderStyle = FormBorderStyle.FixedDialog,
+                    Text = caption,
+                    StartPosition = FormStartPosition.CenterScreen
+                };
+                Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+                TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+                Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+                confirmation.Click += (sender, e) => { prompt.Close(); };
+                prompt.Controls.Add(textBox);
+                prompt.Controls.Add(confirmation);
+                prompt.Controls.Add(textLabel);
+                prompt.AcceptButton = confirmation;
+
+                return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+            }
+        }
+
     }
 }
