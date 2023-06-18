d8aa249f5a0c1217e553f30bd36a8097f34efb28
vincent <118966490+FUFU487@users.noreply.github.com>
2023-06-18 18:14:59 +0800

    login
    Username
    Password

--- ITP4915M_Project/Login.Designer.cs
+++ ITP4915M_Project/Login.Designer.cs
@@ -42,10 +42,9 @@ namespace ITP4915M_Project
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ITP4915M_Project.Properties.Resources.Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(144, 179);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(108, 143);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(273, 362);
+            this.pictureBox1.Size = new System.Drawing.Size(205, 290);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -54,10 +53,9 @@ namespace ITP4915M_Project
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F);
-            this.label1.Location = new System.Drawing.Point(123, 69);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(92, 55);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(299, 28);
+            this.label1.Size = new System.Drawing.Size(238, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "Yummy Restaurant Login";
             // 
@@ -66,10 +64,9 @@ namespace ITP4915M_Project
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Location = new System.Drawing.Point(120, 96);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(90, 77);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(305, 45);
+            this.label2.Size = new System.Drawing.Size(245, 36);
             this.label2.TabIndex = 2;
             this.label2.Text = "make sure your account is secure";
             // 
@@ -77,42 +74,46 @@ namespace ITP4915M_Project
             // 
             this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.txtUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
-            this.txtUserName.Location = new System.Drawing.Point(548, 245);
-            this.txtUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUserName.ForeColor = System.Drawing.Color.Silver;
+            this.txtUserName.Location = new System.Drawing.Point(411, 196);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(415, 29);
+            this.txtUserName.Size = new System.Drawing.Size(311, 23);
             this.txtUserName.TabIndex = 3;
+            this.txtUserName.Text = "UserName";
+            this.txtUserName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtUserName.Enter += new System.EventHandler(this.txtUserName_Enter);
+            this.txtUserName.Leave += new System.EventHandler(this.txtUserName_Leave);
             // 
             // txtPassword
             // 
             this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
-            this.txtPassword.Location = new System.Drawing.Point(548, 314);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Location = new System.Drawing.Point(411, 251);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(415, 29);
+            this.txtPassword.Size = new System.Drawing.Size(311, 23);
             this.txtPassword.TabIndex = 4;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F);
-            this.label3.Location = new System.Drawing.Point(548, 374);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(411, 299);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(166, 24);
+            this.label3.Size = new System.Drawing.Size(132, 18);
             this.label3.TabIndex = 5;
             this.label3.Text = "Forget password ?";
             // 
             // btnlogin
             // 
-            this.btnlogin.Location = new System.Drawing.Point(863, 459);
-            this.btnlogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnlogin.Location = new System.Drawing.Point(647, 367);
             this.btnlogin.Name = "btnlogin";
-            this.btnlogin.Size = new System.Drawing.Size(100, 29);
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
             this.btnlogin.TabIndex = 6;
             this.btnlogin.Text = "Login";
             this.btnlogin.UseVisualStyleBackColor = true;
@@ -120,9 +121,9 @@ namespace ITP4915M_Project
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1136, 611);
+            this.ClientSize = new System.Drawing.Size(852, 489);
             this.Controls.Add(this.btnlogin);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtPassword);
@@ -130,7 +131,6 @@ namespace ITP4915M_Project
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
