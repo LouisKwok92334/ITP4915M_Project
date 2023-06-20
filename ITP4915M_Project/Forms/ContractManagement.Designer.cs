b58f791e6c2e23b6c85e6eec62444c288555fc37
boscochuen011 <Bosco.chuen@gmail.com>
2023-06-20 17:43:52 +0800

    update

--- ITP4915M_Project/Forms/ContractManagement.Designer.cs
+++ ITP4915M_Project/Forms/ContractManagement.Designer.cs
@@ -34,11 +34,6 @@ namespace ITP4915M_Project.Forms
             this.rdoPlanned = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.txtSupplier = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.btnSearch = new System.Windows.Forms.Button();
             this.btnDetail = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
@@ -48,6 +43,7 @@ namespace ITP4915M_Project.Forms
             this.cbInactive = new System.Windows.Forms.CheckBox();
             this.btnReset = new System.Windows.Forms.Button();
             this.btnViewBPA = new System.Windows.Forms.Button();
+            this.cboSupplier = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -60,13 +56,13 @@ namespace ITP4915M_Project.Forms
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 38;
-            this.dataGridView1.Size = new System.Drawing.Size(433, 394);
+            this.dataGridView1.Size = new System.Drawing.Size(433, 380);
             this.dataGridView1.TabIndex = 0;
             // 
             // rdoContract
             // 
             this.rdoContract.AutoSize = true;
-            this.rdoContract.Location = new System.Drawing.Point(22, 84);
+            this.rdoContract.Location = new System.Drawing.Point(22, 113);
             this.rdoContract.Margin = new System.Windows.Forms.Padding(2);
             this.rdoContract.Name = "rdoContract";
             this.rdoContract.Size = new System.Drawing.Size(84, 16);
@@ -78,7 +74,7 @@ namespace ITP4915M_Project.Forms
             // rdoPlanned
             // 
             this.rdoPlanned.AutoSize = true;
-            this.rdoPlanned.Location = new System.Drawing.Point(22, 47);
+            this.rdoPlanned.Location = new System.Drawing.Point(22, 54);
             this.rdoPlanned.Margin = new System.Windows.Forms.Padding(2);
             this.rdoPlanned.Name = "rdoPlanned";
             this.rdoPlanned.Size = new System.Drawing.Size(83, 16);
@@ -91,11 +87,11 @@ namespace ITP4915M_Project.Forms
             // 
             this.groupBox1.Controls.Add(this.rdoContract);
             this.groupBox1.Controls.Add(this.rdoPlanned);
-            this.groupBox1.Location = new System.Drawing.Point(53, 44);
+            this.groupBox1.Location = new System.Drawing.Point(53, 51);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(331, 138);
+            this.groupBox1.Size = new System.Drawing.Size(331, 177);
             this.groupBox1.TabIndex = 32;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Contract Type";
@@ -103,66 +99,16 @@ namespace ITP4915M_Project.Forms
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(51, 263);
+            this.label7.Location = new System.Drawing.Point(52, 322);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(44, 12);
             this.label7.TabIndex = 34;
             this.label7.Text = "Supplier";
             // 
-            // txtSupplier
-            // 
-            this.txtSupplier.Location = new System.Drawing.Point(121, 260);
-            this.txtSupplier.Margin = new System.Windows.Forms.Padding(2);
-            this.txtSupplier.Name = "txtSupplier";
-            this.txtSupplier.Size = new System.Drawing.Size(183, 22);
-            this.txtSupplier.TabIndex = 33;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(121, 352);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(183, 22);
-            this.dateTimePicker1.TabIndex = 35;
-            this.dateTimePicker1.Value = new System.DateTime(2019, 12, 16, 0, 0, 0, 0);
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
-            // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(121, 402);
-            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(2);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(183, 22);
-            this.dateTimePicker2.TabIndex = 36;
-            this.dateTimePicker2.Value = new System.DateTime(2026, 9, 25, 0, 0, 0, 0);
-            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 357);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 12);
-            this.label2.TabIndex = 37;
-            this.label2.Text = "Start";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 410);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(24, 12);
-            this.label3.TabIndex = 38;
-            this.label3.Text = "End";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(48, 456);
+            this.btnSearch.Location = new System.Drawing.Point(48, 447);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(256, 30);
@@ -173,7 +119,7 @@ namespace ITP4915M_Project.Forms
             // 
             // btnDetail
             // 
-            this.btnDetail.Location = new System.Drawing.Point(768, 456);
+            this.btnDetail.Location = new System.Drawing.Point(768, 447);
             this.btnDetail.Margin = new System.Windows.Forms.Padding(2);
             this.btnDetail.Name = "btnDetail";
             this.btnDetail.Size = new System.Drawing.Size(69, 30);
@@ -185,7 +131,7 @@ namespace ITP4915M_Project.Forms
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(50, 310);
+            this.label10.Location = new System.Drawing.Point(52, 378);
             this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(32, 12);
@@ -195,7 +141,7 @@ namespace ITP4915M_Project.Forms
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 205);
+            this.label1.Location = new System.Drawing.Point(52, 259);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 12);
@@ -204,7 +150,7 @@ namespace ITP4915M_Project.Forms
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(121, 203);
+            this.txtSearch.Location = new System.Drawing.Point(120, 256);
             this.txtSearch.Margin = new System.Windows.Forms.Padding(2);
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(183, 22);
@@ -213,7 +159,7 @@ namespace ITP4915M_Project.Forms
             // cxActive
             // 
             this.cxActive.AutoSize = true;
-            this.cxActive.Location = new System.Drawing.Point(120, 310);
+            this.cxActive.Location = new System.Drawing.Point(120, 378);
             this.cxActive.Margin = new System.Windows.Forms.Padding(2);
             this.cxActive.Name = "cxActive";
             this.cxActive.Size = new System.Drawing.Size(54, 16);
@@ -224,7 +170,7 @@ namespace ITP4915M_Project.Forms
             // cbInactive
             // 
             this.cbInactive.AutoSize = true;
-            this.cbInactive.Location = new System.Drawing.Point(203, 310);
+            this.cbInactive.Location = new System.Drawing.Point(198, 378);
             this.cbInactive.Margin = new System.Windows.Forms.Padding(2);
             this.cbInactive.Name = "cbInactive";
             this.cbInactive.Size = new System.Drawing.Size(61, 16);
@@ -234,7 +180,7 @@ namespace ITP4915M_Project.Forms
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(684, 456);
+            this.btnReset.Location = new System.Drawing.Point(404, 447);
             this.btnReset.Margin = new System.Windows.Forms.Padding(2);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(69, 30);
@@ -245,7 +191,7 @@ namespace ITP4915M_Project.Forms
             // 
             // btnViewBPA
             // 
-            this.btnViewBPA.Location = new System.Drawing.Point(404, 456);
+            this.btnViewBPA.Location = new System.Drawing.Point(672, 447);
             this.btnViewBPA.Margin = new System.Windows.Forms.Padding(2);
             this.btnViewBPA.Name = "btnViewBPA";
             this.btnViewBPA.Size = new System.Drawing.Size(78, 30);
@@ -254,11 +200,20 @@ namespace ITP4915M_Project.Forms
             this.btnViewBPA.UseVisualStyleBackColor = true;
             this.btnViewBPA.Click += new System.EventHandler(this.btnViewBPA_Click);
             // 
+            // cboSupplier
+            // 
+            this.cboSupplier.FormattingEnabled = true;
+            this.cboSupplier.Location = new System.Drawing.Point(120, 322);
+            this.cboSupplier.Name = "cboSupplier";
+            this.cboSupplier.Size = new System.Drawing.Size(183, 20);
+            this.cboSupplier.TabIndex = 53;
+            // 
             // ContractManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(874, 505);
+            this.Controls.Add(this.cboSupplier);
             this.Controls.Add(this.btnViewBPA);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.cbInactive);
@@ -268,12 +223,7 @@ namespace ITP4915M_Project.Forms
             this.Controls.Add(this.label10);
             this.Controls.Add(this.btnDetail);
             this.Controls.Add(this.btnSearch);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.dateTimePicker2);
-            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.txtSupplier);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -294,11 +244,6 @@ namespace ITP4915M_Project.Forms
         private System.Windows.Forms.RadioButton rdoPlanned;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox txtSupplier;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnDetail;
         private System.Windows.Forms.Label label10;
@@ -308,5 +253,6 @@ namespace ITP4915M_Project.Forms
         private System.Windows.Forms.CheckBox cbInactive;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Button btnViewBPA;
+        private System.Windows.Forms.ComboBox cboSupplier;
     }
 }
--- ITP4915M_Project/Forms/ContractManagement.cs
+++ ITP4915M_Project/Forms/ContractManagement.cs
@@ -21,7 +21,10 @@ namespace ITP4915M_Project.Forms
 
         private void ContractManagement_Load(object sender, EventArgs e)
         {
-
+            cboSupplier.DataSource = new BindingSource(LoadSupplierNames(), null);
+            cboSupplier.DisplayMember = "Key";
+            cboSupplier.ValueMember = "Value";
+            cboSupplier.SelectedIndex = -1; // 不选择任何项
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
@@ -49,15 +52,8 @@ namespace ITP4915M_Project.Forms
             string searchValue = txtSearch.Text.Trim();
             string contractType = "";
             string status = "";
-            DateTime? startDate = null;
-            DateTime? endDate = null;
 
-            if (dateTimePicker1.Checked)
-                startDate = dateTimePicker1.Value.Date;
-
-            if (dateTimePicker2.Checked)
-                endDate = dateTimePicker2.Value.Date.AddDays(1).AddSeconds(-1);
-
+        
             if (rdoPlanned.Checked)
                 contractType = "Planned";
             else if (rdoContract.Checked)
@@ -70,45 +66,44 @@ namespace ITP4915M_Project.Forms
 
             string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=ITP4915.accdb";
 
-            string query = "SELECT agreement_id, supplier_id, agreement_type, created_at, end_at, status FROM agreement WHERE 1=1 ";
+            string query = "SELECT a.agreement_id AS AgreementId, s.supplier_name, a.agreement_type, a.status " +
+     "FROM agreement a " +
+     "INNER JOIN supplier s ON a.supplier_id = s.supplier_id " + "WHERE 1 = 1";
 
-            if (!string.IsNullOrWhiteSpace(contractType))
-                query += "AND agreement_type LIKE @ContractType ";
 
             if (!string.IsNullOrWhiteSpace(status))
                 query += "AND status = @Status ";
 
-            if (startDate != null && endDate != null)
-                query += "AND (created_at >= @StartDate AND created_at <= @EndDate) ";
+            if (!string.IsNullOrWhiteSpace(contractType))
+                query += "AND a.agreement_type = @ContractType ";
 
             if (!string.IsNullOrWhiteSpace(searchValue))
-                query += "AND (agreement_id LIKE @SearchValue OR supplier_id LIKE @SearchValue) ";
+                query += "AND (a.agreement_id LIKE @SearchValue) ";
 
-            if (!string.IsNullOrWhiteSpace(txtSupplier.Text))
-                query += "AND supplier_id LIKE @SearchValue ";
+            if (!string.IsNullOrWhiteSpace(cboSupplier.Text))
+                query += "AND s.supplier_id = @SupplierId ";
+
+            if (!string.IsNullOrWhiteSpace(contractType))
+                query += "AND a.agreement_type = @ContractType ";
+
 
             try
             {
                 using (OleDbConnection connection = new OleDbConnection(connectionString))
                 using (OleDbCommand command = new OleDbCommand(query, connection))
                 {
-                    if (!string.IsNullOrWhiteSpace(contractType))
-                        command.Parameters.AddWithValue("@ContractType", "%" + contractType + "%");
-
+                   
                     if (!string.IsNullOrWhiteSpace(status))
                         command.Parameters.AddWithValue("@Status", status);
 
-                    if (startDate != null && endDate != null)
-                    {
-                        command.Parameters.AddWithValue("@StartDate", startDate.Value);
-                        command.Parameters.AddWithValue("@EndDate", endDate.Value);
-                    }
-
                     if (!string.IsNullOrWhiteSpace(searchValue))
-                        command.Parameters.AddWithValue("@SearchValue", "%" + searchValue + "%");
+                        command.Parameters.AddWithValue("@SearchValue", searchValue);
 
-                    if (!string.IsNullOrWhiteSpace(txtSupplier.Text))
-                        command.Parameters.AddWithValue("@SearchValue", "%" + txtSupplier.Text.Trim() + "%");
+                    if (!string.IsNullOrWhiteSpace(contractType))
+                        command.Parameters.AddWithValue("@ContractType", contractType);
+
+                    if (!string.IsNullOrWhiteSpace(cboSupplier.Text))
+                        command.Parameters.AddWithValue("@SupplierId", cboSupplier.SelectedValue);
 
                     using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
                     {
@@ -126,13 +121,39 @@ namespace ITP4915M_Project.Forms
             }
         }
 
+        private Dictionary<string, int> LoadSupplierNames()
+        {
+            Dictionary<string, int> supplierNames = new Dictionary<string, int>();
 
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=ITP4915.accdb";
+            string query = "SELECT supplier_id, supplier_name FROM supplier";
 
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                using (OleDbCommand command = new OleDbCommand(query, connection))
+                {
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int supplierId = Convert.ToInt32(reader["supplier_id"]);
+                            string supplierName = reader["supplier_name"].ToString();
+                            supplierNames.Add(supplierName, supplierId);
+                        }
+                    }
+                }
+            }
+
+            return supplierNames;
+        }
 
         private void LoadData()
         {
             string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=ITP4915.accdb";
-            string query = "SELECT agreement_id AS AgreementId, supplier_id, agreement_type, created_at, end_at FROM agreement";
+            string query = "SELECT a.agreement_id AS AgreementId, s.supplier_name, a.agreement_type, a.created_at,a.status " +
+                  "FROM agreement a " +
+                  "INNER JOIN supplier s ON a.supplier_id = s.supplier_id";
 
             try
             {
@@ -158,13 +179,12 @@ namespace ITP4915M_Project.Forms
             LoadData();
 
             txtSearch.Text = "";
-            dateTimePicker1.Checked = false;
-            dateTimePicker2.Checked = false;
             rdoPlanned.Checked = false;
             rdoContract.Checked = false;
-
             cxActive.Checked = false;
             cbInactive.Checked = false;
+            cboSupplier.Text = "";
+
         }
 
         private void btnDetail_Click(object sender, EventArgs e)
